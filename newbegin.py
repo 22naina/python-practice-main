15e147016beafed2f385457e8aa2a03450f9cdd5
Naina Singh <nainasinghrajput14@gmail.com>
2022-10-21 23:53:54 +0700

    python task by adding input function

---
+++ newbegin.py
@@ -0,0 +1,18 @@
+#python datatype with a new lesson
+#Add two numbers
+num1=input("enter an integer x:")
+num2=input("enter an integer y:")
+sum=num1+num2
+print(sum)
+
+#sub two numbers
+num1=input("enter an integer x:")
+num2=input("enter an integer y:")
+sub=num1-num2
+print(sub)
+
+#mul two numbers
+num1=input("enter an integer x:")
+num2=input("enter an integer y:")
+mul=num1*num2
+print(mul)
