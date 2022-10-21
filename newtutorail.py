0edf6c21f21f6e58a866814c756a448e24f6f3ba
Naina Singh <nainasinghrajput14@gmail.com>
2022-10-21 13:35:50 +0700

    first commit

---
+++ newtutorail.py
@@ -0,0 +1,24 @@
+#python datatype start with easy begning
+fav_int=22
+fav_list=["Panda","Koala","Racoon"]
+print(type(fav_int))
+print(type(fav_list))
+
+#python variable ,a trial
+#add two numbers
+num1=12
+num2=4
+print(num1+num2)
+
+#sub two numbers
+num1=44
+num2=63
+print(num1-num2)
+
+#mul two numbers
+num1=90
+num2=12
+print(num1*num2)
+
+
+
