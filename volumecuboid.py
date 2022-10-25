402f778d89751ccc8268652f7d75f432dea8dd2c
Naina Singh <nainasinghrajput14@gmail.com>
2022-10-25 23:03:39 +0700

    volume of cuboid

---
+++ volumecuboid.py
@@ -0,0 +1,6 @@
+#Finding volume of cuboid:
+cuboid_length=int(input("enter any value:"))
+cuboid_volume=int(input("enter any value:"))
+cuboid_height=int(input("enter any value:"))
+volume_cuboid=(cuboid_length*cuboid_volume*cuboid_height)
+print(volume_cuboid)
