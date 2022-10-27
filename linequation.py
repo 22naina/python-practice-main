677881180268ad0572d3e534e1fd867ee67575d1
Naina Singh <nainasinghrajput14@gmail.com>
2022-10-27 13:32:46 +0700

    calculated sum in account after 6 months by line equation.

---
+++ linequation.py
@@ -0,0 +1,7 @@
+#Equation of line(y=mx+c)
+#claculation of  total amount in new accouunt after 6 months:
+starting_value_c=int(input("enter required amount:"))
+adding_value_m=int(input("enter amount added each month:"))
+no_of_months_x=int(input("enter number of months to check total amount :"))
+sum_in_account=(adding_value_m*no_of_months_x)+starting_value_c
+print(sum_in_account)
